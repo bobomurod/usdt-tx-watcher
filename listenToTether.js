190e7df1c1305ef88b5cb40b893c8e72331635f5
Bobomurod <bobomurod.boboev@yandex.ru>
2022-09-15 00:00:08 +0300

    third commit

--- listenToTether.js
+++ listenToTether.js
@@ -1,7 +1,10 @@
 const ethers = require('ethers');
 const usdtABI = require('./usdtABI.json');
+const MongoClient = require('mongodb').MongoClient
 require('dotenv').config();
-
+let mongoUrl = process.env.MONGO_URL;
+console.log(mongoUrl)
+let documentCount = 0;
 async function ethereum() {
   //const usdtAddress = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
   const usdtContractAddress = process.env.USDT_ERC20_TOKEN_CONTRACT;
@@ -17,32 +20,56 @@ async function ethereum() {
     console.log(JSON.stringify(info, null, 4))
   })
 }
+      let db, client;
+
+(
+    async () => {
+      client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+      db = client.db('usdt');
+    }
+)()
 
 async function bnbchain() {
+  let db;
+  let client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
   const usdtContractAddress = process.env.USDT_BEP20_TOKEN_CONTRACT;
   const provider =await  new ethers.providers.WebSocketProvider(`${process.env.CHAINSTACK_WEBSOCKET_URL}${process.env.CHAINSTACK_API_KEY}`);
   const contract = await new ethers.Contract(usdtContractAddress, usdtABI, provider);
+  let tx
+  let txData;
   try {
     await contract.on("Transfer", (from, to, value, event) => {
-    let info = {
+    tx = {
       from: from,
       to: to,
       value: ethers.utils.formatUnits(value, 18),   // только для юсдт на эфире, для бнб беп здесь будет 18
       data: event
     };
-    console.log(JSON.stringify(info, null, 4))
-  })
+    txData = JSON.stringify(tx, null, 4)
+    insertDocument(tx)
+    })
   } catch (e) {
     console.log(e)
   }
 }
 
+async function insertDocument(txData) {
+    try {
+      await db.collection('transactions').insertOne(txData, function(err, result) {
+          if (err) throw err;
+          console.log(documentCount+=1 ,"document inserted at ", new Date());
+      })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
 // ethereum();
 bnbchain()
-  .catch((e)=>{
-    console.log(e)
-  })
-
+//
+// client.close();
+// console.log("Connection closed!");
 
 
 
