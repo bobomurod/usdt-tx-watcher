de0bdf38a20cf6927c782391c663478cb7c7d702
Bobomurod <bobomurod.boboev@yandex.ru>
2022-09-15 21:19:42 +0300

    pm2 autostart when stuck at nearly 35k

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Tether txs watcher',
+      script: 'listenToTether.js',
+      watch: true,
+      instances: 1,
+      exec_mode: 'fork',
+      restart_delay: 14000,
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      out_file: '../services-usdt-logs/out.log',
+      error_file: '../services-usdt-logs/error.log',
+    }]
+};
