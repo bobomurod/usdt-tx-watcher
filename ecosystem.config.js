5c6d0bc79d7a90456c7cb31754943ef0f6d7e3f4
Bobomurod <bobomurod.boboev@yandex.ru>
2022-09-17 23:56:36 +0300

    ecosystem file pripared for restart ervery 90 minutes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
       instances: 1,
       exec_mode: 'fork',
       restart_delay: 14000,
+      cron_restart: "0 */90 * * *",
       log_date_format: 'YYYY-MM-DD HH:mm Z',
       out_file: '../services-usdt-logs/out.log',
       error_file: '../services-usdt-logs/error.log',
