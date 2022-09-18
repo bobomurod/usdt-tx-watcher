486c534c9b28bd418c288f2841d79cfd874b1ed0
Bobomurod <bobomurod.boboev@yandex.ru>
2022-09-18 08:39:54 +0300

    switch to local mongo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: 'Tether txs watcher',
+      name: 'usdt_watcher',
       script: 'listenToTether.js',
       watch: true,
       instances: 1,
       exec_mode: 'fork',
-      restart_delay: 14000,
       cron_restart: '0 */90 * * *',
       log_date_format: 'YYYY-MM-DD HH:mm Z',
       out_file: '../services-usdt-logs/out.log',
