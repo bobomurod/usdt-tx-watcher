3ce7b6ea2cc075e6a31bc0100f0068de847a561d
Bobomurod <bobomurod.boboev@yandex.ru>
2022-09-18 08:59:10 +0300

    ecosystem file update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
       instances: 1,
       exec_mode: 'fork',
       cron_restart: '0 */90 * * *',
+      restart_delay: 60,
       log_date_format: 'YYYY-MM-DD HH:mm Z',
       out_file: '../services-usdt-logs/out.log',
       error_file: '../services-usdt-logs/error.log',
